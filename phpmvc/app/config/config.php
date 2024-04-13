2c6e8a8261e78a572c1be95edf865446bba92eda
sofiahsianipar <sianiparsofiah@gmail.com>
2024-04-13 14:19:24 +0700

    Create config.php

---
+++ phpmvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define ('BASEURL', 'http://localhost/phpmvc/Public/');
+
+// DB
+define('DB_HOST', 'localhost:3308');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
