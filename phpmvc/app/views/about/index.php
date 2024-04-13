d3708049f5c0e3a38cb190218b861429ec1560a2
sofiahsianipar <sianiparsofiah@gmail.com>
2024-04-13 14:20:50 +0700

    Create index.php

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    
+    <h1 class="mt-4"> About me</h1>
+    <img src="<?= BASEURL; ?>/img/Sofiah.jpg"  alt="Saya" width="200" class="rounded-circle shadow">
+    <p> Halo nama saya <?= $data['nama']; ?> umur saya <?= $data['umur']; ?> tahun . Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    
+</div>
